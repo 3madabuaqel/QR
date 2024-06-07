c0d751bf8df81d3a49ebef66749f50c1fcddddc4
3madabuaqel <99624105+3madabuaqel@users.noreply.github.com>
2024-06-07 07:29:48 +0300

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,93 @@
+import tkinter as tk 
+from tkinter import filedialog
+import customtkinter as ctk  
+from PIL import Image,ImageTk
+import qrcode
+
+class App(ctk.CTk):
+    def __init__(self):
+        
+        # window setup
+        ctk.set_appearance_mode('light')
+        super().__init__(fg_color='white')
+        
+        # coustomizition
+        self.title('')
+        self.iconbitmap('empty.ico')
+        self.geometry('500x500')
+        
+        # Entry field
+        self.entry_string = ctk.StringVar()
+        self.entry_string.trace('w', self.create_qr)
+        # make the save method callable from the entryfield button
+        EntryField(self, self.entry_string, self.save)
+        
+        # event
+        # also call it when the enter key is pressed
+        self.bind('<Return>', self.save)
+        
+        # qrcode 
+        self.raw_image = None
+        self.tk_image = None
+        self.qr_image = QrImage(self)
+        
+        # run app
+        self.mainloop()
+        
+    def create_qr(self, *args):
+        current_text = self.entry_string.get()
+        if current_text:
+            self.raw_image = qrcode.make(current_text).resize((200,200))
+            self.tk_image = ImageTk.PhotoImage(self.raw_image)
+            self.qr_image.Update_image(self.tk_image)
+        else:
+            self.qr_image.clear()
+            self.raw_image = None
+            self.tk_image_image = None
+    def save(self, event):
+        if self.raw_image:
+            file_path = filedialog.asksaveasfilename()
+            if file_path:
+                self.raw_image.save(file_path +'.jpg')
+
+class EntryField(ctk.CTkFrame):
+    def __init__(self, parent, entry_string, save_func):
+        super().__init__(master = parent,corner_radius=20,fg_color='#ffffff')
+        self.place(relx=0.5, rely=1, relwidth=1, relheight=0.4, anchor='center')
+
+        # grid layout
+        self.rowconfigure((0,1), weight=1, uniform='a')
+        self.columnconfigure(0, weight=1, uniform='a')
+        
+        # Widgets
+        self.frame = ctk.CTkFrame(self, fg_color='transparent')
+        self.frame.columnconfigure(0, weight=1, uniform='b')
+        self.frame.columnconfigure(1, weight=4, uniform='b')
+        self.frame.columnconfigure(2, weight=2, uniform='b')
+        self.frame.columnconfigure(3, weight=1, uniform='b')
+        self.frame.grid(row=0,column=0)
+        
+        entry = ctk.CTkEntry(self.frame, textvariable=entry_string, fg_color='#2E54E8' ,border_width=0, text_color='white')
+        entry.grid(row=0, column=1 ,sticky='nsew')
+        
+        btn = ctk.CTkButton(self.frame, text='save', fg_color='#2E54E8', hover_color='#4266F1', command=lambda:save_func(''))
+        btn.grid(row=0, column=2 ,sticky='nsew', padx=10) 
+
+class QrImage(tk.Canvas):
+    def __init__(self, parent):
+        super().__init__(master= parent, background='white', bd=0, highlightthickness=0, relief='ridge')
+        self.place(relx=0.5, rely=0.4, width=200, height=200, anchor='center')
+
+    def Update_image(self, image_tk):
+        self.clear()
+        self.create_image(0,0, image=image_tk, anchor='nw')
+        
+    def clear(self):
+        self.delete('all')
+
+
+
+App()
+
+
+
